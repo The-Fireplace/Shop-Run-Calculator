4591c9762ff1ee6599fa25c9b0026067964f8140
The_Fireplace <git@the-fireplace.dev>
2024-04-13 08:29:23 -0500

    Pull the main app render logic out from the tab render logic, create simple tab bar

--- src/app.rs
+++ src/app.rs
@@ -1,10 +1,15 @@
-use std::error::Error;
 use std::io;
 use std::time::Duration;
 
 use crossterm::event;
-use crossterm::event::{Event, KeyCode, KeyEvent, KeyEventKind};
-use ratatui::prelude::{Stylize, Widget};
+use crossterm::event::{Event, KeyCode, KeyEventKind};
+use ratatui::buffer::Buffer;
+use ratatui::Frame;
+use ratatui::layout::{Alignment, Rect};
+use ratatui::prelude::{Line, Span, Stylize, Widget};
+use ratatui::symbols::border;
+use ratatui::widgets::{Block, Borders};
+use ratatui::widgets::block::{Position, Title};
 
 use crate::app::viewer::Viewer;
 use crate::tui::Tui;
@@ -20,8 +25,17 @@ enum Screen {
     Viewer(Viewer),
 }
 
-trait KeyEventHandler {
+trait AppViewBorderDetails {
+    fn get_controls(self) -> Vec<ControlGuide>;
+}
+
+trait AppKeyHandler {
     fn handle_key_event(self, key_code: KeyCode);
+}
+
+struct ControlGuide {
+    instruction: String,
+    key_names: Vec<String>,
 }
 
 impl App {
@@ -35,11 +49,7 @@ impl App {
     pub fn run(&mut self, terminal: &mut Tui) -> io::Result<()> {
         while !self.exit {
             terminal.draw(|frame| {
-                match &self.screen {
-                    Screen::Viewer(viewer) => {
-                        frame.render_widget(viewer, frame.size())
-                    }
-                }
+                self.render_frame(frame);
             })?;
 
             if event::poll(Duration::from_millis(10))? {
@@ -60,5 +70,55 @@ impl App {
         }
 
         Ok(())
+    }
+    
+    fn render_frame(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.size());
+    }
+}
+
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer) where Self: Sized {
+        let title = Title::from(" Shop Run Overview ".bold());
+        let mut base_instructions = match &self.screen {
+            Screen::Viewer(viewer) => viewer.get_controls(),
+        };
+        base_instructions.push(ControlGuide {
+            instruction: "Quit".to_owned(),
+            key_names: vec!["Esc".to_owned()],
+        });
+        let instructions = base_instructions.iter().flat_map(|control| {
+            let mut name = String::new();
+            name.push(' ');
+            name.push_str(&control.instruction);
+            name.push(' ');
+            let mut controls = String::new();
+            controls.push('<');
+            controls.push_str(&control.key_names.join("|"));
+            controls.push('>');
+            if &control.instruction == &base_instructions.last().expect("We're inside an iter over this vec, so last must be defined.").instruction {
+                controls.push(' ');
+            }
+            vec![
+                name.into(),
+                controls.blue().bold(),
+            ]
+        }).collect::<Vec<Span>>();
+
+        let instructions = Title::from(Line::from(instructions));
+        let block = Block::default()
+            .title(title.alignment(Alignment::Center))
+            .title(
+                instructions
+                    .alignment(Alignment::Center)
+                    .position(Position::Bottom),
+            )
+            .borders(Borders::ALL)
+            .border_set(border::THICK);
+        let screen_bounds = block.inner(area);
+        block.render(area, buf);
+        match &self.screen {
+            Screen::Viewer(viewer) => viewer.render(screen_bounds, buf),
+        }
     }
 }
--- src/app/viewer.rs
+++ src/app/viewer.rs
@@ -1,80 +1,62 @@
+mod locations;
+mod shops;
+mod items;
+
 use crossterm::event::KeyCode;
 use ratatui::buffer::Buffer;
-use ratatui::layout::{Alignment, Rect};
-use ratatui::prelude::{Line, Span, Stylize, Widget};
-use ratatui::symbols::border;
-use ratatui::widgets::{Block, Borders};
-use ratatui::widgets::block::{Position, Title};
+use ratatui::layout::{Rect};
+use ratatui::prelude::{Stylize, Widget};
+use ratatui::style::{Color, Style};
+use ratatui::widgets::{Tabs};
 
-use crate::app::KeyEventHandler;
+use crate::app::{AppKeyHandler, AppViewBorderDetails, ControlGuide};
 
-struct ControlGuide {
-    instruction: String,
-    key_names: Vec<String>,
+enum Tab {
+    Locations,
+    Shops,
+    Items,
 }
 
 pub struct Viewer {
-    
+    tab: Tab,
 }
 
 impl Viewer {
     pub fn new() -> Viewer {
         Viewer {
-            
+            tab: Tab::Locations,
         }
     }
 }
 
 impl Widget for &Viewer {
     fn render(self, area: Rect, buf: &mut Buffer) where Self: Sized {
-        let title = Title::from(" Shop Run Overview ".bold());
+        let tabs = Tabs::new(vec!["Locations", "Shops", "Items"])
+            .highlight_style(Style::default().bg(Color::White).fg(Color::Blue).slow_blink())
+            .style(Style::default().white())
+            .select(0);
+        //TODO render tab contents
+        tabs.render(area, buf)
+    }
+}
+
+impl AppViewBorderDetails for &Viewer {
+    fn get_controls(self) -> Vec<ControlGuide> {
         let mut base_instructions = vec![];
-        //TODO per-tab controls
+        //TODO add tab instructions
         base_instructions.push(ControlGuide {
             instruction: "Switch Tabs".to_owned(),
             key_names: vec!["Tab".to_owned()],
         });
-        base_instructions.push(ControlGuide {
-            instruction: "Quit".to_owned(),
-            key_names: vec!["Esc".to_owned()],
-        });
-        let instructions = base_instructions.iter().flat_map(|control| {
-            let mut name = String::new();
-            name.push(' ');
-            name.push_str(&control.instruction);
-            name.push(' ');
-            let mut controls = String::new();
-            controls.push('<');
-            controls.push_str(&control.key_names.join("|"));
-            controls.push('>');
-            if &control.instruction == &base_instructions.last().expect("We're inside an iter over this vec, so last must be defined.").instruction {
-                controls.push(' ');
-            }
-            vec![
-                name.into(),
-                controls.blue().bold(),
-            ]
-        }).collect::<Vec<Span>>();
-        
-        let instructions = Title::from(Line::from(instructions));
-        let block = Block::default()
-            .title(title.alignment(Alignment::Center))
-            .title(
-                instructions
-                    .alignment(Alignment::Center)
-                    .position(Position::Bottom),
-            )
-            .borders(Borders::ALL)
-            .border_set(border::THICK);
-        block.render(area, buf)
+
+        base_instructions
     }
 }
 
-impl KeyEventHandler for &mut Viewer {
+impl AppKeyHandler for &mut Viewer {
     fn handle_key_event(self, key_code: KeyCode) {
-        
         match key_code {
-            
+            //TODO handle tab switching
             _ => {},
         }
     }
